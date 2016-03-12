bba8b366b79aafc3e9e4b971a4f7af11c44ef078
Roel Van Gils <roelvangils@gmail.com>
2016-03-12 19:54:31 +0100

    Did some cleaning up

--- src/jquery.hitMe.js
+++ src/jquery.hitMe.js
@@ -44,7 +44,6 @@
         // Ensure that all other links, buttons and input fields inside container remain functional
         $container.find('a, button, input, textarea').not($target).css({ 'position' : 'relative' , 'z-index' : '9999' });
 
-        console.log($target);
       };
     });
 
